e8429bf7be955d503e7c55c3a1355e9412dcc90a
Strubbelkopp <nils130902@gmail.com>
2026-05-30 20:18:19 +0200

    Replaced flake.nix with devenv

---
+++ devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  languages = {
+    python = {
+      enable = true;
+      package = pkgs.python3.withPackages (ps: [
+        ps.pytest
+        ps.blessed
+      ]);
+    };
+    rust = {
+      enable = true;
+    };
+  };
+}
