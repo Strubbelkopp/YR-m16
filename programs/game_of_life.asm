48038f785100822e3ee7a1b6412a4ce19b653e09
Strubbelkopp <nils130902@gmail.com>
2026-06-01 01:49:57 +0200

    Game of Life rewrite: split display & state buffers, board surrounded by dead ghost cells, pattern loading with glider & glider gun

--- programs/game_of_life.asm
+++ programs/game_of_life.asm
@@ -1,124 +1,137 @@
-; Constants
-@let DISPLAY_BUFFER_A = 0xC000
-@let DISPLAY_BUFFER_B = 0xC780 ; DISPLAY_BUFFER_A + BUFFER_SIZE
 @let WIDTH = 80
 @let HEIGHT = 24
-@let BUFFER_SIZE = 1920 ; WIDTH * HEIGHT
+@let DISPLAY_BUFFER_A = 0xC000
+@let DISPLAY_BUFFER_B = 0xC780 ; + DISPLAY_BUFFER_SIZE
+@let DISPLAY_BUFFER_SIZE = 1920 ; WIDTH * HEIGHT
+@let STATE_BUFFER_A = 0xCF00 ; + DISPLAY_BUFFER_SIZE
+@let STATE_BUFFER_B = 0xD754 ; + STATE_BUFFER_SIZE
+@let STATE_BUFFER_SIZE = 2132 ; (WIDTH + 2) * (HEIGHT + 2)
 @let DEAD = ' '
 @let ALIVE = '@'
-; Memory locations
-@let console_base_reg = 0xF000
-@let active_buffer = 0x1000
-@let hidden_buffer = 0x1002
-; Local variables
-@let tmp = r6
-@let active_cell = r0
-@let new_cell = r1
-@let neighbours = r2
-@let state = r3
-@let i = r4
+@let CONSOLE_BASE_REG = 0xF000
+
+@let front_buffer = 0x1000
+@let back_buffer = 0x1002
+@let current_state_buffer = 0x1004
+@let new_state_buffer = 0x1006
 
 main:
-    mov active_cell, DISPLAY_BUFFER_A
-    store active_cell, console_base_reg
-    store active_cell, active_buffer
-    mov new_cell, DISPLAY_BUFFER_B
-    store new_cell, hidden_buffer
-    call init_buffers
+    call init
+    call copy_state_to_display
+    call swap_buffers
 .loop:
-    call calculate_next_generation
+    call calculate_new_generation
+    call copy_state_to_display
+    call swap_buffers
     jmp .loop
 .end:
     jmp .end
 
-init_buffers:
+@let current_cell = r0
+@let new_cell = r1
+@let state = r5
+@let generation = r6
+init:
+    mov current_cell, DISPLAY_BUFFER_A          ; Initialize display buffer addresses
+    store current_cell, [front_buffer]
+    mov new_cell, DISPLAY_BUFFER_B
+    store new_cell, [back_buffer]
+    store current_cell, CONSOLE_BASE_REG        ; Set console to display front_buffer
+    mov current_cell, STATE_BUFFER_A            ; Initialize state buffer addresses
+    store current_cell, [current_state_buffer]
+    mov new_cell, STATE_BUFFER_B
+    store new_cell, [new_state_buffer]
 .loop:
-    mov state, DEAD
-    storeb state, [active_cell]
-    storeb state, [new_cell]
-    add active_cell, 1
-    add new_cell, 1
-    cmp active_cell, DISPLAY_BUFFER_B   ; Whole buffer filled, once it reaches the start of the 'B' buffer
+    mov state, "  "                             ; Initialize both state buffers with dead cells, two cells at a time
+    store state, [current_cell]
+    store state, [new_cell]
+    add current_cell, 2                         ; Advance to next cell pair
+    add new_cell, 2
+    cmp current_cell, STATE_BUFFER_B            ; Reached end of buffer / beginning of next buffer?
     jne .loop
-.set_patterns:
-    mov state, ALIVE
-    storeb state, [0xC28A]
-    storeb state, [0xC2DB]
-    storeb state, [0xC329]
-    storeb state, [0xC32A]
-    storeb state, [0xC32B]
+.end:
+    call load_pattern
     ret
 
-calculate_next_generation:
-    load active_cell, [active_buffer]
-    load new_cell, [hidden_buffer]
-mov i, 0                                ; Initialize counter
-.loop:
-
+@let current_cell = r0
+@let new_cell = r1
+@let x_counter = r2
+@let y_counter = r3
+@let neighbours = r4
+@let state = r5
+@let neighbour_cell = r6
+calculate_new_generation:
+    load current_cell, [current_state_buffer]   ; Load in cell pointers of (1,1) in state buffer
+    add current_cell, 83 ; WIDTH+3
+    load new_cell, [new_state_buffer]
+    add new_cell, 83 ; WIDTH+3
+    mov y_counter, HEIGHT
+.y_loop:
+    mov x_counter, WIDTH
+.x_loop:
 .calculate_neighbours:
     mov neighbours, 0
-    mov tmp, active_cell
+    mov neighbour_cell, current_cell
 .top_left:
-    sub tmp, 81 ; WIDTH - 1
-    loadb state, [tmp]
+    sub neighbour_cell, 83 ; WIDTH+3
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .top_center
     add neighbours, 1
 .top_center:
-    add tmp, 1
-    loadb state, [tmp]
+    add neighbour_cell, 1
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .top_right
     add neighbours, 1
 .top_right:
-    add tmp, 1
-    loadb state, [tmp]
+    add neighbour_cell, 1
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .center_left
     add neighbours, 1
 .center_left:
-    add tmp, 78 ; WIDTH - 2
-    loadb state, [tmp]
+    add neighbour_cell, 80 ; WIDTH
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .center_right
     add neighbours, 1
 .center_right:
-    add tmp, 2
-    loadb state, [tmp]
+    add neighbour_cell, 2
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .bottom_left
     add neighbours, 1
 .bottom_left:
-    add tmp, 78; WIDTH - 2
-    loadb state, [tmp]
+    add neighbour_cell, 80 ; WIDTH
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .bottom_center
     add neighbours, 1
 .bottom_center:
-    add tmp, 1
-    loadb state, [tmp]
+    add neighbour_cell, 1
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .bottom_right
     add neighbours, 1
 .bottom_right:
-    add tmp, 1
-    loadb state, [tmp]
+    add neighbour_cell, 1
+    loadb state, [neighbour_cell]
     cmp state, ALIVE
     jne .apply_rules
     add neighbours, 1
-
 .apply_rules:
-    loadb state, [active_cell]
+    loadb state, [current_cell]
     cmp state, ALIVE
     jne .dead
 .alive:
-    cmp neighbours, 2                   ; Living cell with < 2 neighbours dies (underpopulation)
+    cmp neighbours, 2                           ; Living cell with < 2 neighbours dies (underpopulation)
     jlt .set_dead
-    cmp neighbours, 3                   ; Living cell with > 3 neighbours dies (overpopulation)
+    cmp neighbours, 3                           ; Living cell with > 3 neighbours dies (overpopulation)
     jgt .set_dead
     jmp .set_new_state
 .dead:
-    cmp neighbours, 3                   ; Dead cell with 3 neighbours lives (reproduction)
+    cmp neighbours, 3                           ; Dead cell with 3 neighbours lives (reproduction)
     jne .set_new_state
 .set_living:
     mov state, ALIVE
@@ -127,23 +140,88 @@ mov i, 0                                ; Initialize counter
     mov state, DEAD
 .set_new_state:
     storeb state, [new_cell]
-
-    add active_cell, 1                  ; Move to next cell
+    ; call print_neighbours
+    add current_cell, 1                         ; Advance to next cell
     add new_cell, 1
-    add i, 1
-    cmp i, BUFFER_SIZE
-    jne .loop
-.swap_buffers:
-    load tmp, [active_buffer]
-    push tmp
-    load tmp, [hidden_buffer]
-    store tmp, [active_buffer]
-    store tmp, console_base_reg
-    pop tmp
-    store tmp, [hidden_buffer]
+    sub x_counter, 1
+    jnz .x_loop
+    add current_cell, 2
+    add new_cell, 2
+    sub y_counter, 1
+    jnz .y_loop
+.end:
     ret
 
-print_neighbours:                       ; Debug function to print the amount of living neighbours around each cell. Call after "calculate_neighbours"
-    add neighbours, 48                  ; Convert number to ASCII
+@let state_buffer_addr = r0
+@let display_buffer_addr = r1
+@let x_counter = r2
+@let y_counter = r3
+@let state = r5
+copy_state_to_display:
+    load state_buffer_addr, [new_state_buffer]  ; Index of (1,1) in state buffer
+    add state_buffer_addr, 83 ; WIDTH+3
+    load display_buffer_addr, [back_buffer]     ; Index of (0,0) in display buffer
+    mov y_counter, HEIGHT
+.y_loop:
+    mov x_counter, WIDTH
+.x_loop:
+    load state, [state_buffer_addr]             ; Write state into back buffer, two cells at a time
+    store state, [display_buffer_addr]
+    add state_buffer_addr, 2                    ; Advance to next cell pair
+    add display_buffer_addr, 2
+    sub x_counter, 2
+    jnz .x_loop
+    add state_buffer_addr, 2                    ; Advance to next row
+    sub y_counter, 1
+    jnz .y_loop
+.end:
+    ret
+
+@let buffer_a = r0
+@let buffer_b = r1
+swap_buffers:
+    load buffer_a, [current_state_buffer]       ; Swap state buffers
+    load buffer_b, [new_state_buffer]
+    store buffer_a, [new_state_buffer]
+    store buffer_b, [current_state_buffer]
+    load buffer_a, [front_buffer]               ; Swap display buffers
+    load buffer_b, [back_buffer]
+    store buffer_a, [back_buffer]
+    store buffer_b, [front_buffer]
+    store buffer_b, CONSOLE_BASE_REG            ; Point console to new front buffer
+    ret
+
+@let new_cell = r1
+@let neighbours = r4
+print_neighbours:                               ; Debug function to print the amount of living neighbours around each cell. Call after "calculate_neighbours"
+    add neighbours, '0'                         ; Convert number to ASCII
     storeb neighbours, [new_cell]
     ret
+
+@let cell_addr = r0
+@let pattern = r1
+@let state = r5
+load_pattern:
+    mov state, ALIVE
+    mov pattern, glider_gun                     ; Load pattern pointer
+    ; mov pattern, glider
+.loop:
+    load cell_addr, [pattern]                   ; Load current cell
+    jz .done                                    ; Look for 0x0000 at end of pattern
+    add cell_addr, STATE_BUFFER_B               ; Add offset to get the actual address
+    storeb state, [cell_addr]
+    add pattern, 2                              ; Advance to next cell in pattern
+    jmp .loop
+.done:
+    ret
+
+glider:
+    @data 0x00, 0x54, 0x00, 0xA7, 0x00, 0xF7, 0x00, 0xF8, 0x00, 0xF9
+    @data 0x00, 0x00
+glider_gun:
+    @data 0x00, 0x6B, 0x00, 0xBB, 0x00, 0xBD, 0x01, 0x03, 0x01, 0x04, 0x01, 0x0B, 0x01, 0x0C, 0x01, 0x19
+    @data 0x01, 0x1A, 0x01, 0x54, 0x01, 0x58, 0x01, 0x5D, 0x01, 0x5E, 0x01, 0x6B, 0x01, 0x6C, 0x01, 0x9B
+    @data 0x01, 0x9C, 0x01, 0xA5, 0x01, 0xAB, 0x01, 0xAF, 0x01, 0xB0, 0x01, 0xED, 0x01, 0xEE, 0x01, 0xF7
+    @data 0x01, 0xFB, 0x01, 0xFD, 0x01, 0xFE, 0x02, 0x03, 0x02, 0x05, 0x02, 0x49, 0x02, 0x4F, 0x02, 0x57
+    @data 0x02, 0x9C, 0x02, 0xA0, 0x02, 0xEF, 0x02, 0xF0
+    @data 0x00, 0x00
